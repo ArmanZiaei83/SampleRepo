f2ed5761fbed8f11bf8947b57488c19518b4b897
a.ziaei <armandoziaeidev@gmail.com>
2022-08-21 19:37:29 +0430

    bugfix: a bug fixed in infra.persistence layer

---
+++ Infrastructure.Persistence/Migrations/Migrations/_202208202202_IntializedDatabase.cs
@@ -0,0 +1,81 @@
+﻿using FluentMigrator;
+using FluentMigrator.Builders;
+using FluentMigrator.Expressions;
+
+namespace Sample.Migrations.Migrations
+{
+    [Migration(202208202202)]
+    public class _202208202202_IntializedDatabase : Migration
+    {
+        public override void Up()
+        {
+            CreateStudentsTable();
+
+            CreateTeachersTable();
+
+            CreateCoursesTable();
+
+            CreateTeacherCoursesTable();
+
+            CreateStudentCoursesTable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("StudentCourses");
+            Delete.Table("TeacherCourese");
+            Delete.Table("Courses");
+            Delete.Table("Teachers");
+            Delete.Table("Students");
+        }
+
+        private void CreateStudentsTable()
+        {
+            Create.Table("Students")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("NationalCode").AsString().NotNullable()
+                .WithColumn("PhoneNumber").AsString().NotNullable();
+        }
+
+        private void CreateTeachersTable()
+        {
+            Create.Table("Teachers")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("NationalCode").AsString().NotNullable()
+                .WithColumn("PhoneNumber").AsString().NotNullable();
+        }
+
+        private void CreateCoursesTable()
+        {
+            Create.Table("Courses")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("StartDateTime").AsDateTime()
+                .WithColumn("EndDateTime").AsDateTime()
+                .WithColumn("StartTime").AsString()
+                .WithColumn("EndTime").AsString();
+        }
+
+        private void CreateTeacherCoursesTable()
+        {
+            Create.Table("TeacherCourses")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("TeacherId").AsInt32().NotNullable()
+                .ForeignKey("FK_TeacherCourses_Teachers", "Teachers", "Id")
+                .WithColumn("CourseId").AsInt32().NotNullable()
+                .ForeignKey("FK_TeacherCourses_Courses", "Courses", "Id");
+        }
+
+        private void CreateStudentCoursesTable()
+        {
+            Create.Table("StudentCourses")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("StudentId").AsInt32().NotNullable()
+                .ForeignKey("FK_StudentCourses_Students", "Students", "Id")
+                .WithColumn("CourseId").AsInt32().NotNullable()
+                .ForeignKey("FK_StudentCourses_Courses", "Courses", "Id");
+        }
+    }
+}
---
+++ Infrastructure.Persistence/Migrations/Runner.cs
@@ -0,0 +1,95 @@
+﻿using System.IO;
+using FluentMigrator.Runner;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    internal class Runner
+    {
+        private static void Main(string[] args)
+        {
+            var options = GetSettings(args, Directory.GetCurrentDirectory());
+
+            var connectionString = options.ConnectionString;
+
+            CreateDatabase(connectionString);
+
+            var runner = CreateRunner(connectionString, options);
+
+            runner.MigrateUp();
+        }
+
+        private static void CreateDatabase(string connectionString)
+        {
+            var databaseName = GetDatabaseName(connectionString);
+            var masterConnectionString = ChangeDatabaseName
+                (connectionString, "master");
+            var commandScript = $"if db_id(N'{databaseName}') is null " +
+                                $"create database [{databaseName}]";
+
+            using var connection = new SqlConnection(masterConnectionString);
+            connection.Open();
+            using (var command = new SqlCommand(commandScript, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+
+            connection.Close();
+        }
+
+        private static string ChangeDatabaseName
+            (string connectionString, string databaseName)
+        {
+            var csb = new SqlConnectionStringBuilder(connectionString)
+            {
+                InitialCatalog = databaseName
+            };
+            return csb.ConnectionString;
+        }
+
+        private static string GetDatabaseName(string connectionString)
+        {
+            return new
+                SqlConnectionStringBuilder(connectionString).InitialCatalog;
+        }
+
+        private static IMigrationRunner CreateRunner
+            (string connectionString, MigrationSettings options)
+        {
+            var container = new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(_ => _
+                    .AddSqlServer()
+                    .WithGlobalConnectionString(connectionString)
+                    .ScanIn(typeof(Runner).Assembly).For.All())
+                .AddSingleton(options)
+                .AddLogging(_ => _.AddFluentMigratorConsole())
+                .BuildServiceProvider();
+            return container.GetRequiredService<IMigrationRunner>();
+        }
+
+        private static MigrationSettings GetSettings
+            (string[] args, string baseDir)
+        {
+            var configurations = new ConfigurationBuilder()
+                .SetBasePath(baseDir)
+                .AddJsonFile
+                    ("appsettings.json", true, true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            var settings = new MigrationSettings();
+            settings.ConnectionString =
+                configurations.GetValue<string>("ConnectionString");
+            return settings;
+        }
+    }
+
+    public class MigrationSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
